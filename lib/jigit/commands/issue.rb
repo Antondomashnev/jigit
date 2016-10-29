97d6c7b46d31c2195b5a0b3c671fea7a1511c4e3
Anton Domashnev <antondomashnev@gmail.com>
2016-10-30 00:07:17 +0200

    Create start issue separate command;

--- lib/jigit/commands/issue.rb
+++ lib/jigit/commands/issue.rb
@@ -4,33 +4,23 @@ require "jigit/jira/jira_config"
 
 module Jigit
   class IssueRunner < Runner
-    self.abstract_command = false
-    self.summary = "Any commands on Jira issue"
+    self.abstract_command = true
+    self.summary = "Abstract command for commands related to JIRA issue"
     self.command = "issue"
 
     def initialize(argv)
-      @action = argv.shift_argument
-      @issue_name = argv.option("name")
       @jira_config = Jigit::JiraConfig.new("antondomashnev+jira1@gmail.com", "Anton2104", "antondomashnevjira1.atlassian.net") # Jigit::JiraConfig.current_jira_config
-      @jira_api_client = Jigit::JiraAPIClient.new(@jira_config) if @jira_config
+      @jira_api_client = Jigit::JiraAPIClient.new(@jira_config, nil, @informator) if @jira_config
       super
     end
 
     def validate!
       super
-      if @action && !%w(stop start).include?(@action)
-        help!("`#{@action}' is not a valid action.")
-      end
       help!("Please setup jira config using `jigit init` before using issue command.") unless @jira_config
-      help!("Please specify JIRA issue. It must not be nil.") unless @issue_name
     end
 
     def self.options
-      [
-        ["stop", "Use this argument if you want to stop working on the feature. For example after checkouting to another branch"],
-        ["start", "Use this argument if you want to start working on the feature. For example after checkouting to that branch"],
-        ["--name=issue_name_on_jira", "Use this argument to provide a JIRA issue name. For example if the project short name is CNI, the issue name could be CNI-101"]
-      ].concat(super)
+      []
     end
 
     def run
---
+++ lib/jigit/commands/start_issue.rb
@@ -0,0 +1,59 @@
+require "jigit/commands/issue"
+
+module Jigit
+  class StartIssueRunner < IssueRunner
+    self.abstract_command = true
+    self.summary = "ommand to put the given JIRA issue to 'In Progress' state"
+    self.command = "start"
+
+    def initialize(argv)
+      @issue_name = argv.option("name")
+      super
+    end
+
+    def validate!
+      super
+      help!("Please specify JIRA issue. It must not be nil.") unless @issue_name
+    end
+
+    def self.options
+      [
+        ["--name=issue_name_on_jira", "Use this argument to provide a JIRA issue name. For example if the project short name is CNI, the issue name could be CNI-101"]
+      ].concat(super)
+    end
+
+    def run
+      self
+      proceed_option = ui.ask_with_answers("Are you going to work on #{issue}?\n", ["yes", "no"])
+      return if proceed_option == "no"
+
+      jira_issue = @jira_api_client.fetch_jira_issue(issue)
+      return unless could_start_working_on_issue?(jira_issue)
+
+      transition_finder = Jigit::JiraTransitionFinder(@jira_api_client.fetch_issue_transitions(jira_issue))
+      to_in_progress_transition = transition_finder.find_transition_to_in_progress
+      unless to_in_progress_transition
+        ui.error("#{issue} doesn't have transition to 'In Progress' status...")
+        return
+      end
+
+      jira_issue.make_transition(to_in_progress_transition.id)
+      ui.inform("#{issue} now is 'In Progress' 💪")
+    end
+
+    private
+
+    def could_start_working_on_issue?(jira_issue)
+      unless jira_issue
+        ui.say("#{issue} doesn't exist on JIRA, skipping...")
+        return false
+      end
+
+      if jira_issue.status.in_progress?
+        ui.say("#{issue} is already in progress...")
+        return false
+      end
+      return true
+    end
+  end
+end
