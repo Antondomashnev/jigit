e3584b055dbd4126f774506ffce868d8be52e0e6
Anton Domashnev <antondomashnev@gmail.com>
2016-10-29 12:46:41 +0200

    describe fetch_issue_transitions in spec;

--- spec/lib/integration/jigit/jira/jira_api_client_spec.rb
+++ spec/lib/integration/jigit/jira/jira_api_client_spec.rb
@@ -9,6 +9,63 @@ describe Jigit::JiraAPIClient do
     basic_client
   }
 
+  describe("fetch_issue_transitions") do
+    let(:jira_issue) {
+      base_issue = JIRA::Resource::Issue.new(jira_client, :attrs => {
+        'id' => '10002',
+        'self' => "#{site_url}/jira/rest/api/2/issue/10002",
+        'fields' => {
+          'comment' => {'comments' => []}
+        }
+      })
+      issue = Jigit::JiraIssue.new(base_issue)
+      issue
+    }
+
+    context("when there is HTTP error") do
+      before do
+        stub_request(:get, site_url + "/jira/rest/api/2/issue/10002/transitions?expand=transitions.fields").
+                    to_return(:status => 405, :body => "<html><body>Some HTML</body></html>")
+      end
+
+      it("returns nil") do
+        jira_api_client = Jigit::JiraAPIClient.new(config, jira_client)
+        expect(jira_api_client.fetch_issue_transitions(jira_issue)).to be_nil
+      end
+    end
+
+    context("when there is no transitions") do
+      before do
+        stub_request(:get, site_url + "/jira/rest/api/2/issue/10002/transitions?expand=transitions.fields").
+                    to_return(:status => 301, :body => "{\"errorMessages\":[\"Transitions Do Not Exist\"],\"errors\":{}}")
+      end
+
+      it("returns nil") do
+        jira_api_client = Jigit::JiraAPIClient.new(config, jira_client)
+        expect(jira_api_client.fetch_issue_transitions(jira_issue)).to be_nil
+      end
+    end
+
+    context("when there are transitions") do
+      before do
+        stub_request(:get, site_url + "/jira/rest/api/2/issue/10002/transitions?expand=transitions.fields").
+                    to_return(:status => 200, :body => get_mock_response('issue_1002_transitions.json'))
+      end
+
+      it("returns correct amount of transitions") do
+        jira_api_client = Jigit::JiraAPIClient.new(config, jira_client)
+        fetched_transitions = jira_api_client.fetch_issue_transitions(jira_issue)
+        expect(fetched_transitions.count).to be == 4
+      end
+
+      it("returns wrapped jira transitions") do
+        jira_api_client = Jigit::JiraAPIClient.new(config, jira_client)
+        fetched_transitions = jira_api_client.fetch_issue_transitions(jira_issue)
+        expect(fetched_transitions).to all( be_instance_of(Jigit::JiraTransition) )
+      end
+    end
+  end
+
   describe("fetch_jira_issue") do
     let(:issue_name) { "ADT-1" }
 
@@ -45,7 +102,7 @@ describe Jigit::JiraAPIClient do
       it("returns wrapped jira issue") do
         jira_api_client = Jigit::JiraAPIClient.new(config, jira_client)
         fetched_issue = jira_api_client.fetch_jira_issue(issue_name)
-        expect(fetched_issue).to be_truthy
+        expect(fetched_issue).to be_instance_of(Jigit::JiraIssue)
       end
     end
   end
