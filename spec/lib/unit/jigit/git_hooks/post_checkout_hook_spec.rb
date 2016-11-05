3f3d33351ea4f228e879053b16c0cab18ba8bcde
Anton Domashnev <antondomashnev@gmail.com>
2016-11-05 23:35:55 +0100

    describe PostCheckoutHook in spec;

---
+++ spec/lib/unit/jigit/git_hooks/post_checkout_hook_spec.rb
@@ -0,0 +1,22 @@
+require "jigit/git_hooks/post_checkout_hook"
+
+describe Jigit::PostCheckoutHook do
+  it "has correct hook lines" do
+    expected_lines = ["#!/usr/bin/env bash",
+                      "checkoutType=$3",
+                      "[[ $checkoutType == 1 ]] && checkoutType='branch' || checkoutType='file'",
+                      "if [ $checkoutType == 'branch' ]; then",
+                      "  newBranchName=`git symbolic-ref --short HEAD`",
+                      "  oldBranchName=`git rev-parse --abbrev-ref @{-1}`",
+                      "  if [ $newBranchName != $oldBranchName ]; then",
+                      "    jigit issue start_issue --name=$newBranchName",
+                      "    jigit issue stop_issue --name=$oldBranchName",
+                      "  fi",
+                      "fi"]
+    expect(Jigit::PostCheckoutHook.hook_lines).to be == expected_lines
+  end
+
+  it "has correct name" do
+    expect(Jigit::PostCheckoutHook.name).to be == "post-checkout"
+  end
+end
