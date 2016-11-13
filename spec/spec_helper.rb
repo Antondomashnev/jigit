7678906b7fb4b64b8a4607ae05b104fc6981eac4
Anton Domashnev <antondomashnev@gmail.com>
2016-11-13 22:47:22 +0100

    Add simplecov-json;

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,11 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 # Needs to be required and started before danger
 require "simplecov"
+require 'simplecov-json'
 SimpleCov.start do
   add_filter "/spec/"
 end
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 def get_mock_response(file, value_if_file_not_found = false)
   file.sub!("?", "_") # we have to replace this character on Windows machine
